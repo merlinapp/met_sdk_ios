16661e18e57b81bd9674fc04de34815ce14dfa1e
Camila Gaitan <camila@dev.merlinjobs.co>
2018-05-10 15:56:54 -0500

    Initial Commit - Create Project

---
+++ MET-SDK/MET-SDK/MET_SDK.h
@@ -0,0 +1,19 @@
+//
+//  MET_SDK.h
+//  MET-SDK
+//
+//  Created by Camila Gaitan on 5/10/18.
+//  Copyright © 2018 Merlin Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MET_SDK.
+FOUNDATION_EXPORT double MET_SDKVersionNumber;
+
+//! Project version string for MET_SDK.
+FOUNDATION_EXPORT const unsigned char MET_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MET_SDK/PublicHeader.h>
+
+
