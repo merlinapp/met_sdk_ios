8dd8b6c82c0827be8af7fb5a62909546378170e2
Camila Gaitán Mosquera <camila@merlinjobs.com>
2018-05-11 11:52:50 -0500

    Update initial Project

---
+++ MerlinMetPod/MerlinMetPod/MerlinMetPod.h
@@ -0,0 +1,19 @@
+//
+//  MerlinMetPod.h
+//  MerlinMetPod
+//
+//  Created by Camila Gaitan Mosquera on 5/11/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MerlinMetPod.
+FOUNDATION_EXPORT double MerlinMetPodVersionNumber;
+
+//! Project version string for MerlinMetPod.
+FOUNDATION_EXPORT const unsigned char MerlinMetPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MerlinMetPod/PublicHeader.h>
+
+
--- MerlinMetPod/MerlinMetPodTests/MerlinMetPodTests.swift
+++ MerlinMetPod/MerlinMetPodTests/MerlinMetPodTests.swift
@@ -1,36 +1,31 @@
 //
-//  MET_SDKTests.swift
-//  MET-SDKTests
+//  MerlinMetPodTests.swift
+//  MerlinMetPodTests
 //
-//  Created by Camila Gaitan on 5/10/18.
-//  Copyright © 2018 Merlin Jobs. All rights reserved.
+//  Created by Camila Gaitan Mosquera on 5/11/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
 //
 
 import XCTest
-@testable import MET_SDK
+@testable import MerlinMetPod
 
-class MET_SDKTests: XCTestCase {
-    
+class MerlinMetPodTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+          // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
