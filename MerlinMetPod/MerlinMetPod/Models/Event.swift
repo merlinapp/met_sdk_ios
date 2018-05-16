261478b18725eca4cb59e116bd4fc803e7921549
Camila Gaitán Mosquera <camila@merlinjobs.com>
2018-05-16 17:45:27 -0500

    Create the network layer to support the new MET pod

---
+++ MerlinMetPod/MerlinMetPod/Models/Event.swift
@@ -0,0 +1,12 @@
+//
+//  Event.swift
+//  MerlinMetPod
+//
+//  Created by Camila Gaitan Mosquera on 5/16/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+}
