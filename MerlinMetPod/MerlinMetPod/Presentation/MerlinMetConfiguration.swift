6c6ae60b66109375412bebf88338759397835e3e
Camila Gaitán Mosquera <camila@merlinjobs.com>
2018-09-04 15:36:37 -0500

    Singleton to test the communication between SDK and the app

---
+++ MerlinMetPod/MerlinMetPod/Presentation/MerlinMetConfiguration.swift
@@ -0,0 +1,24 @@
+//
+//  MerlinMetConfiguration.swift
+//  MerlinMetPod
+//
+//  Created by Camila Gaitan Mosquera on 9/4/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
+//
+
+import Foundation
+
+public class MerlinMetConfiguration: NSObject {
+    
+    static let shared: MerlinMetConfiguration = MerlinMetConfiguration()
+    let URL: String = ""
+    
+    override init() {
+        super.init()
+    }
+    
+    public func initWithURL(URL:String) {
+        self.URL = URL
+    }
+    
+}
