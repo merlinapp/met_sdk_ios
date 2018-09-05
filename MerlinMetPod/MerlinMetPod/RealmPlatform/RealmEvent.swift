da31c66f86f3692d63ad4bb2f38a1da9423dfdda
Mario Acero <marioaceroc@icloud.com>
2018-09-05 14:23:02 -0500

    Add realm manager and object

---
+++ MerlinMetPod/MerlinMetPod/RealmPlatform/RealmEvent.swift
@@ -0,0 +1,20 @@
+//
+//  RealmEvents.swift
+//  MerlinMetPod
+//
+//  Created by Mario Acero on 9/5/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmEvent: Object {
+    
+    @objc dynamic var batchId: String? = nil
+    @objc dynamic var jsonString: String? = nil
+    
+//    override static func primaryKey() -> String? {
+//        return "idEvent"
+//    }
+}
