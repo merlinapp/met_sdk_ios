b5497ca591ac20aeb823034435bc1a0638eb61f5
Mario Acero <marioaceroc@icloud.com>
2018-09-05 15:46:01 -0500

    Add missing files

---
+++ MerlinMetPod/MerlinMetPod/RealmPlatform/RealmEvent.swift
@@ -0,0 +1,16 @@
+//
+//  RealmEvents.swift
+//  MerlinMetPod
+//
+//  Created by Mario Acero on 9/5/18.
+//  Copyright © 2018 Camila Gaitan Mosquera. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmEvent: Object {
+    
+    @objc dynamic var batchId: String? = nil
+    @objc dynamic var jsonString: String? = nil
+}
